4e24e56f2e617f2c2f5adafa1ba0b58ae4b54fb0
Martin <bekzodsanakulov89@gmail.com>
2021-08-12 17:17:16 +0900

    feat: main page develop

--- src/app/components/navbar/navItems.tsx
+++ src/app/components/navbar/navItems.tsx
@@ -64,7 +64,7 @@ export function NavItems() {
    }
 
    return <ListContainer>
-      <NavItem>
+      <NavItem style={{"color": "red"}} >
          <a href="#">Home</a>
       </NavItem>
       <NavItem>
