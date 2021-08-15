ef7f261191ff00a9610cc19ceae8e1120ded450d
martin <martin@comets.kr>
2021-08-16 05:07:45 +0900

    feat: marginer and book process develop

--- src/app/containers/HomePage/index.tsx
+++ src/app/containers/HomePage/index.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import styled from "styled-components";
 import tw from "twin.macro";
+import { BookCard } from "../../components/bookCard";
+import { Marginer } from "../../components/marginer";
 import { Navbar } from "../../components/navbar";
 import { TopSection } from "./topSection";
 
@@ -19,5 +21,6 @@ export function HomePage() {
     return <PageContainer>
         <Navbar />
         <TopSection />
+        <BookCard />
     </PageContainer>;
 }
