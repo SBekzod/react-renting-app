0e234eed0e5afb16163b731c1428902a3576b7f0
Martin <martin@comets.kr>
2021-08-15 20:31:51 +0900

    feat: main page buttons

--- src/app/containers/HomePage/topSection.tsx
+++ src/app/containers/HomePage/topSection.tsx
@@ -5,6 +5,7 @@ import tw from "twin.macro";
 import MclarenCarImg from "../../../assets/images/mclaren-orange-big.png";
 import BlobImg from "../../../assets/images/blob.svg";
 import { SCREENS } from "../../components/responsive";
+import { Button } from "../../components/button";
 
 const TopSectionContainer = styled.div`
   min-height: 400px;
@@ -140,11 +141,18 @@ const StandaloneCar = styled.div`
   }
 `;
 
+const ButtonsContainer = styled.div`
+  ${tw`
+    flex
+    flex-wrap
+    mt-4
+  `};
+`;
 
 export function TopSection() {
   return (
     <TopSectionContainer>
-
+      
       <LeftContainer>
         <Slogan>Rent The Best Qulity Car's With Us</Slogan>
         <Description>
@@ -152,8 +160,12 @@ export function TopSection() {
           at the best price for you and get the best quality cars for as long as
           you like
         </Description>
+        <ButtonsContainer>
+          <Button text="Book Your Ride" />
+          <Button theme="filled" text="Sell Your Car" />
+        </ButtonsContainer>
       </LeftContainer>
-
+      
       <RightContainer>
         <BlobContainer>
           <img src={BlobImg} />
