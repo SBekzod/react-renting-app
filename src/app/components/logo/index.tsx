cdc449c0194acf28a0b1fb9dfd73dd5aeab6e8c8
martin <martin@comets.kr>
2021-08-18 02:01:39 +0900

    fix: footer component develop

--- src/app/components/logo/index.tsx
+++ src/app/components/logo/index.tsx
@@ -1,53 +1,52 @@
 import React from "react";
-import styled, { css } from "styled-components";
+import styled from "styled-components";
 import tw from "twin.macro";
 
-import CartLogoImg from '../../../assets/images/car-logo.png';
+import CarLogoImg from "../../../assets/images/car-logo.png";
+import CarLogoDarkImg from "../../../assets/images/car-logo-dark.png";
 
 interface ILogoProps {
-    color?: 'white' | 'dark'
+  color?: "white" | "dark";
+  bgColor?: "white" | "dark";
 }
 
-
 const LogoContainer = styled.div`
-    ${tw`
+  ${tw`
     flex
     items-center
-    `};
+  `};
 `;
 
 const LogoText = styled.div`
-    ${tw`
-        text-xl
-        md:text-2xl
-        text-black
-        m-1
+  ${tw`
+    text-xl
+    md:text-2xl
+    font-bold
+    text-black
+    m-1
     `};
-
-    ${({ color }: any) => color === 'white' ?
-        css`${tw`text-white`}`
-       :
-        css`${tw`text-black`}`
-    }
-`;
+  ${({ color }: any) => (color === "white" ? tw`text-white` : tw`text-black`)}
+` as any;
 
 const Image = styled.div`
-    width:auto;
-    ${tw`h-6 md:h-9`};
-    img {
-        width: auto;
-        height: 100%;
-    }
+  width: auto;
+  ${tw`h-6 md:h-9`};
+
+  img {
+    width: auto;
+    height: 100%;
+  }
 `;
 
 export function Logo(props: ILogoProps) {
+  const { color, bgColor } = props;
 
-    const {color } = props;
-
-    return <LogoContainer>
-        <Image>
-            <img src={CartLogoImg} />
-        </Image>
-        <LogoText color={color || 'dark'}>Yourcar.</LogoText>
-    </LogoContainer>;
+  return (
+    <LogoContainer>
+      <Image>
+        <img src={bgColor === "dark" ? CarLogoDarkImg : CarLogoImg} />
+      </Image>
+      <LogoText color={color || "dark"}>Yourcar.</LogoText>
+    </LogoContainer>
+  );
 }
